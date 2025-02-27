4d7930a127d55c950db3d1194acd69cde4b874b3
Bob <Bob@example.com>
2025-02-27 15:50:40 +0900

    bug fix

--- src/main/java/com/example/soccerCommunity/controller/MessageController.java
+++ src/main/java/com/example/soccerCommunity/controller/MessageController.java
@@ -1,12 +1,10 @@
 package com.example.soccerCommunity.controller;
 
-import com.example.soccerCommunity.dto.CustomOAuth2User;
 import com.example.soccerCommunity.dto.MessageRequestDto;
 import com.example.soccerCommunity.dto.MessageResponseDto;
 import com.example.soccerCommunity.service.MessageService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -21,11 +19,8 @@ public class MessageController {
 
     @MessageMapping("/chat")
     @SendTo("/topic/chat")
-    public MessageResponseDto sendChatMessage(@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
-                                              MessageRequestDto requestDto) {
+    public MessageResponseDto sendChatMessage(MessageRequestDto requestDto) {
 
-        String username = customOAuth2User.getUsername();
-
-        return messageService.processMessage(username, requestDto);
+        return messageService.processMessage(requestDto);
     }
 }
--- src/main/java/com/example/soccerCommunity/service/MessageService.java
+++ src/main/java/com/example/soccerCommunity/service/MessageService.java
@@ -2,25 +2,13 @@ package com.example.soccerCommunity.service;
 
 import com.example.soccerCommunity.dto.MessageRequestDto;
 import com.example.soccerCommunity.dto.MessageResponseDto;
-import com.example.soccerCommunity.entity.UserInfo;
-import com.example.soccerCommunity.repository.UserInfoRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
 
-    private final UserInfoRepository userInfoRepository;
+    public MessageResponseDto processMessage(MessageRequestDto requestDto) {
 
-    public MessageService(UserInfoRepository userInfoRepository){
-
-        this.userInfoRepository = userInfoRepository;
-    }
-
-    public MessageResponseDto processMessage(String username, MessageRequestDto requestDto) {
-
-        UserInfo userInfo = userInfoRepository.findByUser_Username(username);
-        String nickname = userInfo.getNickname();
-
-        return new MessageResponseDto(nickname, requestDto.getContent());
+        return new MessageResponseDto(requestDto.getNickname(), requestDto.getContent());
     }
 }
